2edc75a1b46976542fa2e454bd58d4a00188a873
Alex Viney <alex.viney@biteable.com>
2018-11-05 11:04:38 +1100

    2nd test

--- lyrics.test.js
+++ lyrics.test.js
@@ -3,3 +3,7 @@ const Lyrics = require("./lyrics");
 test("first test", () => {
   expect(new Lyrics().calculate(1)).toBe(1);
 });
+
+test("second test", () => {
+  expect(new Lyrics().calculate(2)).toBe(2);
+});
